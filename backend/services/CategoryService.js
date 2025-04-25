92e2dde4a87d43ee881590292439cefa5cc76bb3
Bernardo Plottegher <bernardoplottegher@gmail.com>
2025-04-24 22:54:56 -0300

    ANT-44
    Adicionado comentários ao CategoryService

--- backend/services/CategoryService.js
+++ backend/services/CategoryService.js
@@ -1,11 +1,13 @@
+// Importa a função runQuery para executar consultas no banco de dados
 const { runQuery } = require("../utils/queryHelper");
 
 class CategoryService {
 
+    // Busca todas as categorias e seus grupos associados no banco de dados.
     async getAll() {
+        // Executa a consulta SQL identificada por "select_groups_categories"
         return runQuery("select_groups_categories");
     }
-
 }
-
+// Exporta a classe CategoryService para ser utilizada em outras partes da aplicação
 module.exports = CategoryService;
