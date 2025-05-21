da918c73eb5ae1089bfd14ea1933953b2467e89c
Fergrenteski <luizgrenfer@gmail.com>
2025-05-21 13:49:28 -0300

    fix-merge

--- backend/services/ProductService.js
+++ backend/services/ProductService.js
@@ -19,6 +19,17 @@ class ProductService {
     async getProductsByUserId(listId, userId) {
         return runQuery("select_products_by_user_id", [userId, listId])
     }
+    async getTotalCategories(listId) {
+        return runQuery("select_total_categories_by_list_id", [listId])
+    }
+
+    async getTopCategoriesByUsers(listId) {
+        return runQuery("select_total_user_categories_by_list_id", [listId])
+    }
+
+    async getTotalByUsers(listId) {
+        return runQuery("select_total_users_by_list_id", [listId])
+    }
 }
 // Exporta a classe ProductService
 module.exports = ProductService;
