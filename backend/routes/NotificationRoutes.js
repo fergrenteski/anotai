33255a1af4f16b9d5c343387a78b487cdd3cf208
Fergrenteski <luizgrenfer@gmail.com>
2025-06-14 10:36:09 -0300

    feat:
     - Atualizado rotas de notificação

--- backend/routes/NotificationRoutes.js
+++ backend/routes/NotificationRoutes.js
@@ -6,15 +6,15 @@ const router = express.Router();
 
 
 // Rotas de Notificação
-router.get("/notification", verificarToken, (req , res) => NotificationController.getAll(req, res));
+router.get("/", verificarToken, (req , res) => NotificationController.getAll(req, res));
 
 // Marcar como lido
-router.put("/notification", verificarToken, (req , res) => NotificationController.markAllAsRead(req, res));
-router.put("/notification/:id", verificarToken, (req , res) => NotificationController.markAsRead(req, res));
+router.put("/", verificarToken, (req , res) => NotificationController.markAllAsRead(req, res));
+router.put("/:id", verificarToken, (req , res) => NotificationController.markAsRead(req, res));
 
 // Excluir notificação
-router.delete("/notification", verificarToken, (req , res) => NotificationController.deleteAll(req, res));
-router.delete("/notification/:id", verificarToken, (req , res) => NotificationController.delete(req, res));
+router.delete("/", verificarToken, (req , res) => NotificationController.deleteAll(req, res));
+router.delete("/:id", verificarToken, (req , res) => NotificationController.delete(req, res));
 
 // Exporta classe  router
 module.exports = router;
